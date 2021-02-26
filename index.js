0b0808e80321fb479a224135405059e0728bbec4
tonvalduga <tonvalduga@terra.com.br>
2021-02-26 20:07:42 -0300

    atualiz\aqndo lacmingo login e JQuery

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ var token = window.localStorage.getItem('Token')
 if (token == null) {
     console.log("token nulo")
 
-    window.location.href = "auth/login/login.html"
+    window.location.href = "login/login.html"
 
 
 } else {
     console.log("token não é nulo")
-    window.location.href = "tarefas/tarefas.html"
+    window.location.href = "cadastro/cadastro.html"
 
 }
